17e335d1a63356ff56ca6e4f7d634f919f290a98
강효선 <kanghyoseon@ganghyoseon-ui-MacBook-Pro.local>
2021-02-08 16:01:17 +0900

    first commit

--- README.md
+++ README.md
@@ -1,19 +1,20 @@
 ## 케이스 주제
 
-Q. 검색어를 입력하는 동시에 엔터키, 검색 버튼 누를 필요 없이 결과값을 즉시 반영할 수 있는 순간 검색 기능을 구현하십시오.
+Q. on, off 기능 (toggle 기능)을 가지는 버튼 리스트 컴포넌트를 구현하십시오.
 
 
 ## 기능 요구사항
 
-1. 키보드 이벤트를 지연시간(debounce 기능)을 통해 request 횟수를 줄인다.
-![요구사항 첫번째](./src/solution/presenter/instant-search/assets/instant_search_scope1.png)
+1. 각 버튼이 선택이 되었는지 안되었는지를 확인할 수 있다.
+![요구사항 첫번째](./src/solution/presenter/toggle_button/assets/toggle_button_scope1.png)
 
 
-2. 디자인 템플릿을 변경할 수 있도록 템플릿을 분리한다.
+2. 여러개의 버튼 중 하나의 버튼만 toggle 할 수 있다.
+![요구사항 두번째](./src/solution/presenter/toggle_button/assets/toggle_button_scope2.png)
 
 
 ## 기능 작동 이미지
-![example_image](./instant-search-example.png)
+![example_image](./toggle_button-example.png)
 
 
 ## 실행 방법 / 문제 풀이 방법
@@ -25,16 +26,13 @@ Run `npm run dev`
 
 
 ## 문제
-- q1. debounce 기능을 구현하시오.
+- q1. 데이터에 따른 버튼리스트를 가로로 출력하시오.
 
-- q2. configuration을 참고하여 input element를 생성하시오. (기능과 디자인을 분리하기 위한 방법)
+- q2. 한개의 버튼만이 on이 될 수 있도록 하시오.
 
-- q3. debounce 기능을 통해 가져온 데이터를 외부로 전달한다. 
-
-- q4. Promise를 사용하여 검색 키워드에 맞는 데이터를 가져와 리스트를 출력하시오. 
+- q3. 선택된 버튼의 index를 application으로 전달 하시오.
 
 
 ## 주요 학습 키워드
-- 지연시간을 적용하여 마지막 이벤트만 발생시키는 기능 (debounce) 
-- request api를 대신하기 위한 Promise 활용 (test를 위한 mock data 활용)
-- 디자인 부분과 기능을 분리하기 위한 방법
+- 버튼 스타일 적용 
+- 컴포넌트와 어플리케이션과의 통신
--- src/index.js
+++ src/index.js
@@ -1,43 +1,20 @@
 // Import stylesheets
 import './style.css';
-import { InstantSearch } from './solution/presenter/instant-search';
-import { retrieveWordList } from './solution/presenter/instant-search/mock/word';
+import { ToggleButton } from './question/toggle-button';
 
 
 const bootstrap = () => {
-    // instant search setup
-    const instantSearch = new InstantSearch({
-        selector: '#instant-search',
-        css: 'instant-search-input',
-        placeholder: 'please enter keyword',
-        changeEvent: (eventText) => {
-            // 해당 단어에 맞는 데이터를 출력
-            getData(eventText);
+    const buttonData = [
+        'Bold', 'Italic', 'Underline'
+    ]
+    const toggleButton = new ToggleButton({
+        selector: '#toggle-button',
+        data: buttonData,
+        changeEvent: (event) => {
+            alert(`selected index: ${event}, label: ${buttonData[event]}`);
         }
-    });
-
-    // 최초에 전체 리스트를 출력.
-    getData();
+    })
 }
 
-const displayWordList = (selector, data) => {
-    let itemList = '';
-    for (let i = 0; i < data.length; i++) {
-        itemList += `
-        <div>
-            <span>${data[i].word}</span>
-        </div>`
-    }
-    selector.innerHTML = itemList;
-}
-
-const getData = (targetWord = '') => {
-    // q4. Promise를 사용하여 검색 키워드에 맞는 데이터를 가져와 리스트를 출력하시오.
-    // TODO: Write JS code here!'
-    retrieveWordList(targetWord)
-        .then((result) => {
-            displayWordList(document.querySelector('#wordlist'), result);
-        });
-}
 
 bootstrap();
